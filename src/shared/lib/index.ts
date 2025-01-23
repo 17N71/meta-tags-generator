9058c55bdc435714ada36d14b5995033e8e9068e
17N71 <narek07012020@gmail.com>
2025-01-23 16:51:19 +0400

    reinit husky, code higihliter create, craete loader, create textarea field, add tabs

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,30 +1,28 @@
-import { transformerCopyButton } from '@rehype-pretty/transformers'
-import { type ClassValue } from "clsx"
-import { codeToHtml } from "shiki"
-import { twMerge } from "tailwind-merge"
-import { highlightProps } from "../types"
-import {  clsx } from "clsx"
+import { transformerCopyButton } from "@rehype-pretty/transformers";
+import { codeToHtml } from "shiki";
+import { twMerge } from "tailwind-merge";
+import { highlightProps } from "../types";
+import { clsx } from "clsx";
 
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+export function cn(...inputs: string[]) {
+  return twMerge(clsx(inputs));
 }
 
 export async function highlight({
-code,
-lang,
-theme = 'night-owl'
-}:highlightProps) {
+  code,
+  lang,
+  theme = "night-owl"
+}: highlightProps) {
   const out = await codeToHtml(code, {
     lang,
     theme,
     transformers: [
       transformerCopyButton({
-        visibility: 'always',
-        feedbackDuration: 3_000,
-      }),
+        visibility: "always",
+        feedbackDuration: 3_000
+      })
     ]
-  })
+  });
 
-  return out
+  return out;
 }
